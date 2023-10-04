b664d7b57e5ba7d0c55c835dea6ea894e2d23c50
Kourva <118578799+Kourva@users.noreply.github.com>
2023-10-04 03:15:37 +0000

    Import file

---
+++ Libraries/imports.py
@@ -0,0 +1,45 @@
+#*# Standard library imports
+
+# To open web URLs
+import webbrowser
+
+#*#  Kivy and Plyer Related third party imports
+
+# To build Kivy app
+from kivy.app import App
+
+# To build kivy syntax files
+from kivy.lang import Builder
+
+# To use animation on widgets
+from kivy.animation import Animation
+
+# To use clock and time related stuff
+from kivy.clock import Clock
+
+# To use kivy Basic canvas stuff
+from kivy.graphics import Color, Rectangle, Ellipse, BoxShadow
+
+# To use line and border in canvas
+from kivy.graphics import Line, RoundedRectangle
+
+# To use screen manager for multi-screen apps
+from kivy.uix.screenmanager import Screen, ScreenManager
+
+# To use Label in app
+from kivy.uix.label import Label
+
+# To use image in app
+from kivy.uix.image import Image
+
+# To use button and button behavior in app
+from kivy.uix.button import Button
+
+# To use button behavior in widgets
+from kivy.uix.behaviors import ButtonBehavior
+
+# To use grid layout in app
+from kivy.uix.gridlayout import GridLayout
+
+# To use float layout in app
+from kivy.uix.floatlayout import FloatLayout
